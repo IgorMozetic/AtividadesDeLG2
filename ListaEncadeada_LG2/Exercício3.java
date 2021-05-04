3c47bb7945d7c55d29e7655213929a8e6cf18fa9
Igor Mozetic <71463737+IgorMozetic@users.noreply.github.com>
2021-05-04 00:41:09 -0300

    Lista Encadeada

--- ListaEncadeada_LG2/Exercício3.java
+++ ListaEncadeada_LG2/Exercício3.java
@@ -1,8 +1,8 @@
-package Exercícios;
+package ExercÃ­cios;
 
 import java.util.LinkedList;
 
-public class Exercício3 {
+public class ExercÃ­cio3 {
 
 	public static void main(String[] args) {
 		
@@ -14,11 +14,11 @@ public class Exercício3 {
 		lista.add(6);
 		lista.add(8);
 		
-		System.out.println( "Os elementos presentes na lista são: " + lista );
+		System.out.println( "Os elementos presentes na lista sÃ£o: " + lista );
 		
 		for( int contador = 0; contador < lista.size() ; contador++ )	{			
 			soma = soma + lista.get( contador );
 		}
-		System.out.println( "A soma dos elementos da lista é: " + soma );
+		System.out.println( "A soma dos elementos da lista Ã©: " + soma );
 	}
 }
